a29f86bb60dbcd3fa2b20a28cf86fe44e6c716b8
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-03 17:10:59 +0900

    [refactoring] exception case

--- SimilarityChecker/checker.cpp
+++ SimilarityChecker/checker.cpp
@@ -6,9 +6,15 @@ using namespace std;
 class SimilarityChecker {
 public:
 	int inspectLength(const string& s1, const string& s2) {
+		AssertOnInvalid(s1, s2);
+		return 0;
+	}
+
+private:
+	void AssertOnInvalid(const std::string& s1, const std::string& s2)
+	{
 		if (s1.empty() || s2.empty()) {
 			throw length_error("The length of string should be more than 0.");
 		}
-		return 0;
 	}
 };
