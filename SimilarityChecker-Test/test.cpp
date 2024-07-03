46f8b4a2d2f322eb217dbc5c063b510aeecd58d2
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-04 08:43:04 +0900

    [refactoring] use fixture on test

--- SimilarityChecker-Test/test.cpp
+++ SimilarityChecker-Test/test.cpp
@@ -4,24 +4,27 @@
 
 using namespace std;
 
-TEST(SimilarityCheckerTest, ZeroLengthException) {
-	SimilarityChecker checker;
+class SimilarityCheckerFixture : public testing::Test {
+public:
+	SimilarityChecker checker{ };
+};
+
+TEST_F(SimilarityCheckerFixture, ZeroLengthException) {
 	EXPECT_THROW(checker.inspectLength("ASD", ""), length_error);
 }
 
-TEST(SimilarityCheckerTest, MaxPoints) {
-	SimilarityChecker checker;
+TEST_F(SimilarityCheckerFixture, MaxPoints) {
 	EXPECT_EQ(60, checker.inspectLength("ASD", "DSA"));
 }
 
-TEST(SimilarityCheckerTest, ZeroPoints) {
+TEST_F(SimilarityCheckerFixture, ZeroPoints) {
 	SimilarityChecker checker;
 	EXPECT_EQ(0, checker.inspectLength("A", "BB"));
 	EXPECT_EQ(0, checker.inspectLength("AA", "B"));
 }
 
-TEST(SimilarityCheckerTest, PartialPoints) {
-	SimilarityChecker checker;
+
+TEST_F(SimilarityCheckerFixture, PartialPoints) {
 	EXPECT_EQ(20, checker.inspectLength("AAABB", "BAA"));
 	EXPECT_EQ(30, checker.inspectLength("AA", "AAE"));
 	EXPECT_EQ(20, checker.inspectLength("BAA", "AAABB"));
