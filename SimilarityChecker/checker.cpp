27bbe631dd5a904b9ae63b163d29484c20607cf1
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-04 09:57:17 +0900

    [refactoring] max alpha point case

--- SimilarityChecker/checker.cpp
+++ SimilarityChecker/checker.cpp
@@ -8,9 +8,23 @@ public:
 	int inspectAlphabet(const string& s1, const string& s2) {
 		AssertOnInvalidArguments(s1, s2);
 
-		if (s1 == "ASD" && s2 == "DSA") return 40;
+		int totalCnt = 0;
+		int sameCnt = 0;
 
-		return 0;
+		bool alphaHit[26] = { false, };
+		for (const char& ch : s1) {
+			int idx = ch - 'A';
+			if (!alphaHit[idx]) totalCnt++;
+			alphaHit[idx] = true;
+		}
+		for (const char& ch : s2) {
+			int idx = ch - 'A';
+			if (!alphaHit[idx]) totalCnt++;
+			else sameCnt++;
+			alphaHit[idx] = true;
+		}
+
+		return (int)((float)MAX_ALPHA_POINT * (float)totalCnt / (float)sameCnt);
 	}
 
 private:
@@ -28,4 +42,6 @@ private:
 		}
 		return false;
 	}
+
+	const int MAX_ALPHA_POINT = 40;
 };
