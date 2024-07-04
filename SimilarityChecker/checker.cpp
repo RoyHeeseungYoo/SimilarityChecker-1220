a56bd1cba12b8ea3eed7e13618a6a3a520bcbd2d
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-04 09:38:51 +0900

    [refactoring] exception case

--- SimilarityChecker/checker.cpp
+++ SimilarityChecker/checker.cpp
@@ -6,9 +6,24 @@ using namespace std;
 class SimilarityChecker {
 public:
 	int inspectAlphabet(const string& s1, const string& s2) {
-		if (s1[0] < 'A' || s1[0] > 'Z'
-			|| s2[0] < 'A' || s2[0] > 'Z') {
+		AssertOnInvalidArguments(s1, s2);
+
+		return 0;
+	}
+
+private:
+	void AssertOnInvalidArguments(const std::string& s1, const std::string& s2)
+	{
+		if (isNonCapital(s1) || isNonCapital(s2)) {
 			throw invalid_argument("");
 		}
+	}
+
+	bool isNonCapital(const std::string& str)
+	{
+		for (auto ch : str) {
+			if (ch < 'A' || ch > 'Z') return true;
+		}
+		return false;
 	}
 };
